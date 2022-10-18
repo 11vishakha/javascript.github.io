79845b6b4d16324c6183ca0728efbc2f8c994e4b
11vishakha <vishakhamhasaye04@gmail.com>
2022-10-19 00:40:30 +0530

    added

---
+++ practice3.js
@@ -0,0 +1,57 @@
+for(i=1;i<=10;i++)
+if(i%2!=0){
+    console.log(i)
+}
+
+for(i=1;i<=10;i++){
+    console.log(i*2)
+}
+
+num1=10
+num2=20
+num3=30
+total=num1>num2 && num2>num1 && num3>num2
+console.log(total)
+
+num1=20
+num2=80
+mult=num1*num2
+console.log('two number multiplication   '+mult)
+
+//if else
+marks=50
+if(marks>60){
+    console.log('you are pass')
+}
+else{
+    console.log('you are failed')
+}
+
+
+//else if ladder
+marks=99
+if(marks>90){
+    console.log('passed with grade A')
+}
+else if(marks>80){
+    console.log('passed with grade B')
+}
+else if(marks>70){
+    console.log('passed with grade C')
+}
+else if(marks>50){
+    console.log('passed with grade D')
+}
+else(marks>35);{
+    console.log('failed')
+}
+
+
+//for
+if(marks>50){
+    console.log("greater than 46")
+}
+
+for(n=0;n<=10;n=n+3){
+    console.log(n)
+}

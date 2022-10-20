28739dd9c3bcd1dae7a12dcaa6b6ab4fbe26f209
11vishakha <vishakhamhasaye04@gmail.com>
2022-10-20 18:04:45 +0530

    add

--- prompt.js
+++ prompt.js
@@ -1,12 +1,10 @@
 
 let sum=0
-let n=prompt('enter the value of n');
+let n = prompt('enter the value of n');
 Number.parseInt(n);
 
-for(i=0;i<5;i++){
+for(i=0;i<4;i++){
     sum+=(i+1);
    console.log(i+1)
 }
 console.log('sum of first number '+n + "natural number is "+sum)
-document.write('sum of first number :' +n),
-document.write('natural number is :' +sum)
---
+++ returnfunct.js
@@ -0,0 +1,9 @@
+
+
+function Numbers(a,b){
+    total =a+b;
+    return total;
+}
+let addition = Numbers(3,4)
+console.log('addition of two values '+total)
+  
